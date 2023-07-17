3828763893a79908e788db3ebe1ad5f996c81020
devcxl <64475363+devcxl@users.noreply.github.com>
2023-07-18 00:54:58 +0800

    optimize: EnableGlobalExceptionHandler

--- src/main/java/cn/devcxl/common/exception/annotation/EnableGlobalExceptionHandler.java
+++ src/main/java/cn/devcxl/common/exception/annotation/EnableGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package cn.devcxl.common.exception.annotation;
 
+import cn.devcxl.common.exception.controller.CustomErrorController;
 import cn.devcxl.common.exception.handler.GlobalExceptionHandler;
 import org.springframework.context.annotation.Import;
 
@@ -9,10 +10,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * 开启全局错误处理 以及自定义错误接口
+ *
  * @author devcxl
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import(GlobalExceptionHandler.class)
+@Import({GlobalExceptionHandler.class, CustomErrorController.class})
 public @interface EnableGlobalExceptionHandler {
 }
